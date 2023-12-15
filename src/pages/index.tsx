f443ba799326c9f77bd8ed028f86d7d1b66603d0
tzptzptzptzptzp <git@tzp.pink>
2023-12-15 23:36:23 +0900

    ⚡️ 別タブで開かないように変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,6 @@ export default function Home() {
               <Link
                 className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
                 href={`/blog/${blog.id}`}
-                target="_blank"
               >
                 <h3 className="text-2xl font-bold">{blog.title}</h3>
                 <div className="text-lg">{blog.description}</div>
